93e92c8cd6b79996f1d3369196ecccef2d3c898b
dy696786 <dy696786@gmail.com>
2020-05-26 15:44:08 +0900

    Spinner 기본 사용법

---
+++ app/src/main/java/com/example/spinner_20200526/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.spinner_20200526;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    public Context mContxt = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
